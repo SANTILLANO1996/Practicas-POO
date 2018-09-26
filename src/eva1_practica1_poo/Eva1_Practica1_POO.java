a28db56e09b6031acd0037f7f705e8016c7ce488
santillano1996 <luissantillano1996@gmail.com>
2018-09-26 16:46:54 -0600

    practica1

---
+++ src/eva1_practica1_poo/Eva1_Practica1_POO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_practica1_poo;
+
+/**
+ *
+ * @author Luis Fernando Martínez Santillano    #18550371
+ */
+public class Eva1_Practica1_POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Hello world!!!");
+         System.out.println("See you later!!!");
+
+    }
+    
+}
